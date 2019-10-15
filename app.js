ec58fd9f353ac27789a960a0b27dcde4b42edcf1
Trisha <trishadasgupta48@gmail.com>
2019-10-15 23:16:33 +0530

    file system

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 var fs = require('fs')
 var argv = require('yargs').argv
 
-fs.writeFileSync(file[0],'You are awesome')
+if(!argv.filename){
+    console.log('Please enter filename.')
+}
+else{
+    fs.open(argv.filename+'.txt','wx',(err,f)=>{
+        if(err) console.log('File already exists.')
+        else{
+            fs.write(f,'You are awesome',(err)=>{
+            if(!err){
+                fs.appendFileSync('filenames.txt',argv.filename+"\n")
+                console.log('File created succesfully.')
+            }
+            
+        })}
+    })
+}
